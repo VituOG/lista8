72922eba81d283f832acbc6cef06949c001a9aed
VituOG <vitor.carv2007@gmail.com>
2025-02-11 13:05:19 -0300

    Initial commit

---
+++ fdfd.js
@@ -0,0 +1,70 @@
+let num = 1
+while (num <=10 ){
+    console.log(num)
+    num++
+}
+
+//2
+
+// let  nume = Number(prompt("digite um numero"))
+let  nume =7
+let mult = 1
+while (mult <= 10){
+    console.log(nume * mult)
+    mult++
+}
+
+//3
+
+let numer = 10
+while (numer >=1 ){
+    console.log(numer)
+    numer--
+}
+
+//4
+
+// let senha = Number(prompt("qual sua senha?"))
+let senha = 12345
+while (senha != "12345"){
+    senha = prompt("try again")
+    if(senha == "12345"){
+        console.log("nice bt")
+    }
+}
+
+//5
+
+let numero = 50
+while (numero <=100 ){
+    console.log(numero)
+    numero++
+}
+
+//6
+
+let numero1 = 0
+while (numero1 <=100 ){
+    console.log(numero1)
+    numero1+=5
+}
+
+//7
+
+let java = "eu gosto de javascript"
+let msg = 1
+while (msg <=5){
+    console.log(java)
+    msg++
+}
+
+//8
+
+// let vitor = (prompt("insira um nome"))
+// let vez = Number(prompt("insita um numero"))
+let vitor = "vitor"
+let vez = 50000000000000000
+while(vez >=1){
+    console.log(vitor)
+    vez--
+}
---
+++ j.js
@@ -0,0 +1,4 @@
+let font = 1
+while(font <10){
+    console.log(font)
+}
